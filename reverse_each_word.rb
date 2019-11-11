959d923ea2260b8e1399eb6a1e9147352b5887d2
Thomas Donovan <tdonovan79@gmail.com>
2019-11-11 01:45:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(to_reverse)
   word_array = to_reverse.split(" ")
-  word_array.collect{|x| x.reverse}
+  word_array = word_array.collect{|x| x.reverse}
   return word_array.join(" ")
 end
